b7641817d645609df075cb68d8f3da35f409c95b
zahra <zahrapoonevala@berkeley.edu>
2020-10-21 01:10:13 +0700

    part 2

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,168 @@
+package bearmaps;
+
+import java.util.*;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>  {
+//    private int size;
+    private ArrayList<Node> minHeap;
+    private HashMap<T, Integer> getItem; //need this for the changePriority func
+
+    private class Node {
+        T value;
+        double keyPriority;
+
+        Node(T V, double K){
+        value = V;
+        keyPriority = K;
+    }
+
+    }
+
+        public ArrayHeapMinPQ() {
+//            size = 0;
+            minHeap = new ArrayList<Node>();
+            getItem = new HashMap<T, Integer>();
+        }
+
+    /** @source https://algs4.cs.princeton.edu/24pq/MinPQ.java.html */
+        private void swap(int k, int i){
+            getItem.replace(minHeap.get(k).value, i);
+            getItem.replace(minHeap.get(i).value, k);
+            Node swap = minHeap.get(k);
+            minHeap.set(k, minHeap.get(i));
+            minHeap.set(i, swap);
+        }
+
+    /** @source cs61b 2020 ds6 lec21 heaps and pq pseudocode */
+    private int parent(int k) {
+        return (k - 1) / 2;
+    }
+
+    /** @source cs61b 2020 ds6 lec21 heaps and pq pseudocode */
+    private int leftChild(int k) {
+        return (k * 2) + 1;
+    }
+
+    /** @source cs61b 2020 ds6 lec21 heaps and pq pseudocode */
+    private int rightChild(int k) {
+        return (k * 2) + 1 + 1;
+    }
+
+    /** @source cs61b 2020 ds6 lec21 heaps and pq pseudocode */
+        private void swim(int k) {
+            int tempP = parent(k);
+            if (minHeap.get(tempP).keyPriority > minHeap.get(k).keyPriority){
+                swap(k, tempP);
+                swim(parent(k));
+            }
+
+        }
+
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException();
+        } else {
+            Node newNode = new Node(item, priority);
+            minHeap.add(newNode);
+            getItem.put(item, minHeap.size() - 1);
+            swim(minHeap.size() - 1);
+
+        }
+
+        }
+
+
+    @Override
+    public boolean contains(T item) {
+        return getItem.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        if(minHeap.size() == 0){
+            throw new NoSuchElementException();
+        }
+        T answer;
+        answer = minHeap.get(0).value;
+        return answer;
+
+    }
+
+//    private int sinkChildChoice(int k) {
+//        int tempLeftChild = leftChild(k);
+//        int tempRightChild = rightChild(k);
+//        int swapChild;
+//
+//        if (minHeap.get(tempLeftChild).keyPriority <= minHeap.get(tempRightChild).keyPriority) {
+//            swapChild = tempLeftChild;
+//        } else {
+//            swapChild = tempRightChild;
+//        }
+//        return swapChild;
+//    }
+
+    /** @source https://algs4.cs.princeton.edu/24pq/MinPQ.java.html */
+    private void sink(int k) {
+        int tempLeftChild = leftChild(k);
+        int tempRightChild = rightChild(k);
+        int swapChild;
+        while(tempLeftChild < size()) {
+                swapChild = tempLeftChild;
+            if (tempRightChild < size() && (minHeap.get(tempRightChild).keyPriority < minHeap.get(swapChild).keyPriority)) {
+                swapChild = tempRightChild;
+            }
+
+            if (minHeap.get(k).keyPriority < minHeap.get(swapChild).keyPriority) {
+                return;
+            }
+            swap(k, swapChild);
+            sink(swapChild);
+
+        }
+
+    }
+
+
+    @Override
+    public T removeSmallest() {
+        if(size() == 0) {
+            throw new NoSuchElementException();
+        }
+            T result = this.getSmallest();
+            getItem.remove(result);
+            swap(0, minHeap.size() - 1);
+            minHeap.remove(minHeap.size() - 1);
+            sink(0);
+
+        return result;
+
+    }
+
+    @Override
+    public int size() {
+        return minHeap.size();
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException();
+        }
+        int itemChange = getItem.get(item);
+        double prevP = minHeap.get(itemChange).keyPriority;
+        minHeap.get(itemChange).keyPriority = priority;
+
+        if (prevP > priority){
+            swim(itemChange);
+        }
+        sink(itemChange);
+
+    }
+
+    int getX (T item) {
+        return getItem.get(item);
+    }
+
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,163 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.In;
 import org.junit.Test;
+import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHeapMinPQTest {
+
+    @Test
+
+    public void testAddFunction() {
+        ArrayHeapMinPQ<String> testAdd = new ArrayHeapMinPQ<>();
+        testAdd.add("A", 3);
+        testAdd.add("B", 1);
+        testAdd.add("C", 2);
+        testAdd.add("D", 6);
+        testAdd.add("E", 10);
+        assertEquals(5, testAdd.size());
+    }
+
+    @Test
+    public void testGetSmallestFunction() {
+        ArrayHeapMinPQ<Integer> test2 = new ArrayHeapMinPQ<>();
+        test2.add(10, 3);
+        test2.add(9, 1);
+        test2.add(8, 2);
+        test2.add(7, 6);
+        test2.add(6, 10);
+        assertEquals((Integer)9, test2.getSmallest());
+    }
+    @Test
+    public void testRemoveSmallestFunction() {
+        ArrayHeapMinPQ<Integer> test3 = new ArrayHeapMinPQ<>();
+        test3.add(6, 100);
+        test3.add(7, 101);
+        test3.add(8, 103);
+        test3.add(11, 108);
+        test3.add(9, 1108);
+        assertEquals((Integer)6, test3.removeSmallest());
+        assertEquals(4, test3.size());
+        assertEquals((Integer)7, test3.removeSmallest());
+        assertEquals(3, test3.size());
+    }
+
+    @Test
+    public void testChangePriorityFunction() {
+        ArrayHeapMinPQ<Integer> test4 = new ArrayHeapMinPQ<>();
+        test4.add(5, 3);
+        test4.add(6,2);
+        test4.add(7,1);
+        test4.add(8,5);
+        test4.add(12,10);
+        assertEquals(5, test4.size());
+        assertEquals((Integer) 7, test4.getSmallest());
+        test4.changePriority(7,4);
+        assertEquals((Integer) 6, test4.getSmallest());
+
+    }
+
+    @Test
+    public void testCombinedFunctions() {
+        ArrayHeapMinPQ<Integer> test5 = new ArrayHeapMinPQ<>();
+        test5.add(5, 3);
+        test5.add(6,2);
+        test5.add(7,1);
+        test5.add(20,0.1);
+        test5.add(30,0.001);
+        test5.add(40,0.5);
+        test5.add(500,1000);
+        test5.add(9,100);
+        test5.add(12,20);
+        assertEquals(9, test5.size());
+        assertFalse(test5.contains(1));
+        assertTrue(test5.contains(30));
+        assertEquals((Integer)30,test5.removeSmallest());
+        assertFalse(test5.contains(30));
+
+        test5.changePriority(20, 1.1);
+
+        assertEquals((Integer) 40, test5.getSmallest());
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testException1() {
+        ArrayHeapMinPQ<Integer> test7 = new ArrayHeapMinPQ<>();
+        test7.add(1,5);
+        test7.add(1,4);
+
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void testException2() {
+        ArrayHeapMinPQ<Integer> test8 = new ArrayHeapMinPQ<>();
+        test8.add(1,5);
+        test8.add(2,4);
+        test8.removeSmallest();
+        test8.removeSmallest();
+        test8.getSmallest();
+
+    }
+
+    @Test (expected = NoSuchElementException.class)
+    public void testException3() {
+        ArrayHeapMinPQ<Integer> test9 = new ArrayHeapMinPQ<>();
+        test9.add(1,5);
+        test9.add(2,4);
+        test9.add(3,8);
+        test9.changePriority(4,5);
+
+    }
+
+
+    /** @source cs61b fall 2020 Lab 5 */
+    private static void printTimingTable(List<Integer> Ns, List<Double> times, List<Integer> opCounts) {
+        System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
+        System.out.printf("------------------------------------------------------------\n");
+        for (int i = 0; i < Ns.size(); i += 1) {
+            int N = Ns.get(i);
+            double time = times.get(i);
+            int opCount = opCounts.get(i);
+            double timePerOp = time / opCount * 1e6;
+            System.out.printf("%12d %12.2f %12d %12.2f\n", N, time, opCount, timePerOp);
+        }
+    }
+
+
+    @Test
+    public void timeTestAdd() {
+        List<Integer> Ns1 = new ArrayList<>();
+        List<Double> times = new ArrayList<>();
+        List<Integer> opCounts = new ArrayList<>();
+        Random temp = new Random (10);
+        ArrayHeapMinPQ<Integer> test = new ArrayHeapMinPQ<>();
+
+        Stopwatch sw1 = new Stopwatch();
+        for(int i = 0; i<= 1000000; i = i + 1){
+            test.add(i,temp.nextInt());
+            //List<Point> xPoints = randomPoints(i);
+
+            //for(int m = 0; m<10000; m++){
+            //new KDTree(randomPoints);
+            // System.out.println(pRand);
+            times.add(sw1.elapsedTime());
+            Ns1.add(i);
+            opCounts.add(i);
+        }
+        printTimingTable( Ns1, times,  opCounts);
+
+    }
+
+
+
 }
